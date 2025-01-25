38b744838340108850000751ae5664de74753ec3
rabakkar <rozabdullah49@gmail.com>
2025-01-25 23:34:05 +0300

    Delete serial_monitor directory

--- serial_monitor/serial_monitor.ino
+++
@@ -1,7 +0,0 @@
-void displayResults(int distance, int fsoil) {
-    Serial.print("Distance: ");
-    Serial.print(distance);
-    Serial.print(" cm    Humidity: ");
-    Serial.print(fsoil);
-    Serial.println("%");
-}
