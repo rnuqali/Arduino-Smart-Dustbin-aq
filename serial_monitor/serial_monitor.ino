a8d5d2e8b2bda830ebc5aad7a79737c223bab548
Arwa-fahd <arwa1425aaa@gmail.com>
2025-01-25 22:43:00 +0300

    added serial monitor functionality

---
+++ serial_monitor/serial_monitor.ino
@@ -0,0 +1,7 @@
+void displayResults(int distance, int fsoil) {
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.print(" cm    Humidity: ");
+    Serial.print(fsoil);
+    Serial.println("%");
+}
